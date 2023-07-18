4b119291f88ac1bcdd2b8c4f64a5af815705d61a
Rahul Anand <RRSIMT.RAHUL@GMAIL.COM>
2023-07-18 19:04:18 +0400

    Update welcome

--- src/react-chat/src/components/Welcome.js
+++ src/react-chat/src/components/Welcome.js
@@ -10,7 +10,7 @@ const Welcome = () => {
     };
     return (
         <main className="welcome">
-            <h2>Chat App</h2>
+            <h2>My Chat App</h2>
             <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
             <p>Sign in with Google to chat with with your friends.</p>
             <button className="sign-in">
